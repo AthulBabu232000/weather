38bbf96aa7b9d365ab1baadc95426c4f55282369
webdesign4alll <asbupsilonviroidno1@gmail.com>
2021-08-02 00:34:21 +0530

    issue resolved

--- src/App.js
+++ src/App.js
@@ -23,11 +23,11 @@ function App() {
   }
 
   const dateBuilder = (d) =>{ 
-    let months = ["", "January", "February", "March", "April", "June", "July", "August", "September", "October", "November", "December"];
+    let months = ["January", "February", "March", "April", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"];
     let day = days[d.getDay()];
     let date = d.getDate();
-    let month = months[d.getMonth()];
+    let month = months[d.getMonth() - 1];
     let year = d.getFullYear();
     return `${day} ${date} ${month} ${year}`
   }
